9e71e8d5a0737515abd452fce19012283e9b770a
depthso <86912923+depthso@users.noreply.github.com>
2023-04-01 07:59:36 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+import os, random
+
+### Config:
+
+wallpapers_PATH = "/home/owl/Pictures/wallpapers"
+
+######
+
+wallpaper = random.choice(os.listdir(wallpapers_PATH))
+wallpaper_PATH = f"{wallpapers_PATH}/{wallpaper}"
+
+string = f"gsettings set org.gnome.desktop.background picture-uri-dark file:///{wallpaper_PATH}"
+
+os.system(string)
